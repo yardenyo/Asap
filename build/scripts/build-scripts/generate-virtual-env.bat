2f35033d9f1d492a74e9b0c26f4bdb0e841b8b6f
Aviram Dayan <aviram26@gmail.com>
2021-11-23 20:49:47 +0200

    - build scripts

--- build/scripts/build-scripts/generate-virtual-env.bat
+++ build/scripts/build-scripts/generate-virtual-env.bat
@@ -9,21 +9,17 @@ python -m venv .venv
 echo Activating Python virtual environment...
 call .venv\Scripts\activate.bat
 
-:: Install pip packages
-:: IMPORTANT NOTICE!!! - Any newly used package during development should be added here for deployment time
 echo Installing %PROJECT_NAME% Python dependencies...
 python -m pip install -q -r %PROJECT_NAME%/code/server/requirements.txt
 
 :: Export Python dependencies
 echo Export %PROJECT_NAME% Python dependencies - generating requirements.txt file...
 python -m pip freeze > output\resources\python_requirements\requirements.txt
-copy %PROJECT_NAME%\build\scripts\deploy-scripts\deploy-django.bat output\resources\python_requirements\
-copy %PROJECT_NAME%\build\scripts\deploy-scripts\populate-db.bat output\resources\python_requirements\
 
 :: Generate and copy Django static files
 echo Generate and copy Django static files
-python %PROJECT_NAME%\code\server\src\manage.py collectstatic --no-input --clear
-xcopy %PROJECT_NAME%\code\server\src\static output\%PROJECT_NAME%\static\static\ /eqv
+python %PROJECT_NAME%\code\server\src\asap\manage.py collectstatic --no-input --clear
+xcopy %PROJECT_NAME%\code\server\src\asap\static output\%PROJECT_NAME%\static\static\ /eqv
 
 echo Deactivating Python virtual environment...
 call deactivate
