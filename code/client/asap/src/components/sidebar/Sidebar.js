b5f153c68328af819d6160b4e6434a19d3971007
Aviram Dayan <aviram26@gmail.com>
2021-11-27 22:46:52 +0200

    https://gitlab.com/sapir-exlab/asap/-/issues/20 - small fixes

--- code/client/asap/src/components/sidebar/Sidebar.js
+++ code/client/asap/src/components/sidebar/Sidebar.js
@@ -1,22 +1,9 @@
-import React, { useMemo } from 'react';
-import { Link } from 'react-router-dom';
-import { useIntl } from 'react-intl';
+import React from 'react';
 import useRouting from '../../services/routing/hooks/useRouting';
 import style from './Sidebar.module.css';
 
 const Sidebar = () => {
-    const { formatMessage } = useIntl();
-    const { routesMetadataForRole } = useRouting();
-
-    const links = useMemo(
-        () =>
-            routesMetadataForRole.map(({ id, path, i18nKey }) => (
-                <div key={id}>
-                    <Link to={path}>{formatMessage({ id: i18nKey })}</Link>
-                </div>
-            )),
-        [routesMetadataForRole, formatMessage]
-    );
+    const { links } = useRouting();
 
     return <div className={style.sidebarContainer}>{links}</div>;
 };
--- code/client/asap/src/layouts/BaseLayoutRoutes.js
+++ code/client/asap/src/layouts/BaseLayoutRoutes.js
@@ -1,32 +1,17 @@
-import React, { useEffect, useMemo } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import { useLocation, useNavigate } from 'react-router';
-import PrivateRoute from '../services/routing/PrivateRoute';
+import React, { useEffect } from 'react';
+import { Routes } from 'react-router-dom';
+import { useNavigate } from 'react-router';
 import useRouting from '../services/routing/hooks/useRouting';
 
-const routesPerRole = routesMetadataForRole =>
-    routesMetadataForRole.map(({ id, path, Component }) => (
-        <Route
-            key={id}
-            path={path}
-            element={
-                <PrivateRoute>
-                    <Component />
-                </PrivateRoute>
-            }
-        />
-    ));
-
 const BaseLayoutRoutes = () => {
-    const location = useLocation();
     const navigate = useNavigate();
-    const { routesMetadataForRole } = useRouting();
-    const routes = useMemo(() => routesPerRole(routesMetadataForRole), [routesMetadataForRole]);
+    const { routes, initialRoute } = useRouting();
 
     useEffect(() => {
-        location.hash === '' && navigate(routesMetadataForRole[0].path, { replace: true });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        if (!!initialRoute) {
+            navigate(initialRoute, { replace: true });
+        }
+    }, [initialRoute, navigate]);
 
     return <Routes>{routes}</Routes>;
 };
