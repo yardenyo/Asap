1a728d12cd5167c049bc5230f224a45be84e0d6d
saarrozen <saarroze0@gmail.com>
2021-11-28 13:11:10 +0200

    Logo finish

---
+++ code/client/asap/src/components/logo/Logo.js
@@ -0,0 +1,9 @@
+import SapirLogo from '../../SapirLogo.png'
+const Logo = () => {
+    return <div>
+        <img src={SapirLogo} width={150} height={50}/>
+
+    </div>;
+};
+
+export default Logo;
