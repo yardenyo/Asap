48f7b1867d9fcb6610605273d456db0daefb63ad
Galaloush <galaloush@gmail.com>
2021-11-28 14:57:23 +0200

    - did all README requirements - made a GET function to create a mock response

--- code/server/src/asap/core/views.py
+++ code/server/src/asap/core/views.py
@@ -1,17 +1,30 @@
-from rest_framework import status, generics
-from rest_framework.decorators import api_view, renderer_classes
+from rest_framework import status
+from rest_framework.decorators import api_view, renderer_classes, authentication_classes, permission_classes
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from core.decorators import authorized_roles
-from core.models import Version, Profile
 from core.roles import Role
-from core.serializers import VersionSerializer, ProfileSerializer
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+@authentication_classes([])
+@permission_classes([])
+def dummy_without_auth(request):
+    return Response({'status': 'ok'}, status=status.HTTP_200_OK)
+
+
+@api_view(['GET'])
+@renderer_classes([JSONRenderer])
+@authorized_roles(roles=[Role.ASAP_ADMIN])
+def dummy_with_admin_auth(request):
+    return Response({'status': 'ok'}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
 @renderer_classes([JSONRenderer])
-@authorized_roles(roles=[Role.ASAP_ADMIN, Role.ASAP_DEPT_HEAD, Role.ASAP_APPT_CHAIR])
+@authorized_roles(roles=[Role.ASAP_ADMIN, Role.ASAP_DEPT_HEAD])
 def get_current_user(request):
     user = request.user
     roles = [row['name'] for row in user.groups.values('name')]
@@ -24,54 +37,19 @@ def get_current_user(request):
     return Response(content, status=status.HTTP_200_OK)
 
 
-@api_view(['POST'])
-@renderer_classes([JSONRenderer])
-@authorized_roles(roles=[Role.ASAP_ADMIN, Role.ASAP_DEPT_HEAD, Role.ASAP_APPT_CHAIR])
-def get_current_version(request):
-    version = Version.objects.get(pk=1)
-    serializer = VersionSerializer(version)
-    return Response(serializer.data, status=status.HTTP_200_OK)
-
-
 @api_view(['GET'])
 @renderer_classes([JSONRenderer])
-def candidates_table(request):
-    user_id = request.user.id
-    response = [
+def active_requests(request):
+    admin_id = request.user.id
+
+    requests = [
         {
-            'candidate': 1,
+            'admin': admin_id,
+            'candidate': 3,
             'requestedRank': "Manager",
-            'submissionDate': "25-11-2021",
-            'stageNumber': 3,
-            'stageName': "interview",
+            'submissionDate': "10.11.21",
+            'stageNumber': 1,
+            'stageName': "request",
         }
     ]
-
-    return Response(response, status=status.HTTP_200_OK)
-
-
-@api_view(['GET'])
-@renderer_classes([JSONRenderer])
-def get_table_data(request):
-    user_id = request.user.id
-    response = [
-        {
-            'candidate': 1,
-            'requestedRank': "Manager",
-            'submissionDate': "25-11-2021",
-            'stageNumber': 3,
-            'stageName': "interview",
-        }
-    ]
-
-    return Response(response, status=status.HTTP_200_OK)
-
-
-class ProfileList(generics.ListCreateAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
-
-
-class ProfileDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Profile.objects.all()
-    serializer_class = ProfileSerializer
+    return Response(requests, status=status.HTTP_200_OK)
