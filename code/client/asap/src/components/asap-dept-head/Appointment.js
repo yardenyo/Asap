dfa6ff23414e3843963af473466366ed4f6238f5
yardenyo <59290717+yardenyo@users.noreply.github.com>
2021-12-17 18:21:24 +0200

    -conflict solving

--- code/client/asap/src/components/asap-dept-head/Appointment.js
+++ code/client/asap/src/components/asap-dept-head/Appointment.js
@@ -1,39 +1,39 @@
-import React from 'react';
+import React from "react";
 import Selection from "../shared/Selection";
 import {useIntl} from "react-intl";
 import FileSelection from "../shared/FileSelection";
 import {Button} from "@mui/material";
 
 const Appointment = () => {
-    const { formatMessage } = useIntl();
+    const {formatMessage} = useIntl();
     const candidate = ["Yarden", "Itay"];
     const rank = ["Admin", "Head"];
     return (
-    <div>
-        {formatMessage({ id: 'routes.asap-dept-head-appointment' })}
-        <Selection
-            id="1"
-            title={formatMessage({ id: 'Select_component.candidate.placeholder' })}
-            options={candidate}
-        />
-        <Selection
-            id="2"
-            title={formatMessage({ id: 'Select_component.rank.placeholder' })}
-            options={rank}
-        />
-        <FileSelection
-            title={formatMessage({ id: 'file-selection.malag.placeholder' })}
-            id="1"
-        />
-        <FileSelection
-            title={formatMessage({ id: 'file-selection.initiated.placeholder' })}
-            id="1"
-        />
+        <div>
+            {formatMessage({id: 'routes.asap-dept-head-appointment'})}
+            <Selection
+                id="1"
+                title={formatMessage({id: 'Select_component.candidate.placeholder'})}
+                options={candidate}
+            />
+            <Selection
+                id="2"
+                title={formatMessage({id: 'Select_component.rank.placeholder'})}
+                options={rank}
+            />
+            <FileSelection
+                title={formatMessage({id: 'file-selection.malag.placeholder'})}
+                id="1"
+            />
+            <FileSelection
+                title={formatMessage({id: 'file-selection.initiated.placeholder'})}
+                id="2"
+            />
 
-        <Button type="submit" variant="contained" color="success">
-            {formatMessage({ id: 'file-selection.submitbutton.children' })}
-        </Button>
-    </div>);
+            <Button type="submit" variant="contained" color="success">
+                {formatMessage({id: 'file-selection.submitbutton.children'})}
+            </Button>
+        </div>);
 };
 
 export default Appointment;
