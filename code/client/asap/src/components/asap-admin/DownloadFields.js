5621ea4b76b47f2ad417330c5c70eaed135e3042
itays <itayshlomy@gmail.com>
2021-12-19 10:46:18 +0200

    download and remarks component fixed with prettier

--- code/client/asap/src/components/asap-admin/DownloadFields.js
+++ code/client/asap/src/components/asap-admin/DownloadFields.js
@@ -1,32 +1,26 @@
-import {useState} from "react";
-import "./DownloadFields.css";
+import { useState } from 'react';
+import './DownloadFields.css';
 import { useIntl } from 'react-intl';
 
-const DownloadFields=({title})=>{
+const DownloadFields = ({ title }) => {
     const [remarks, setRemarks] = useState('');
     const { formatMessage } = useIntl();
 
-     const getField = () => {
-         console.log(remarks)
-     }
+    const getField = () => {
+        console.log(remarks);
+    };
 
-  return (
-    <div className={'fieldContainer'}>
-        <div className={'titleField'}>
-            {title}
+    return (
+        <div className={'fieldContainer'}>
+            <div className={'titleField'}>{title}</div>
+            <div className={'download'}>
+                <button onClick={getField}>{formatMessage({ id: 'download-field-admin.button-text.text' })}</button>
             </div>
-        <div className={'download'}>
-            <button onClick={getField} >
-                {formatMessage({id:'download-field-admin.button-text.text'})}
-            </button>
+            <div className={'Remarks'}>{formatMessage({ id: 'download-field-admin.remarks.text' })}</div>
+            <div className={'textBox'}>
+                <textarea onChange={event => setRemarks(event.target.value)} />
+            </div>
         </div>
-        <div className={'Remarks'}>
-        {formatMessage({id:'download-field-admin.remarks.text'})}
-        </div>
-        <div className={'textBox'}>
-            <textarea onChange={event => setRemarks(event.target.value)}/>
-        </div>
-    </div>
-  );
-}
-export default DownloadFields ;
+    );
+};
+export default DownloadFields;
