6421fa289bba99f39548174cca48127bc556a63c
saarrozen <saarroze0@gmail.com>
2022-01-03 18:26:31 +0200

    details for admin page

--- code/client/asap/src/services/state/AsapContextProvider.js
+++ code/client/asap/src/services/state/AsapContextProvider.js
@@ -2,6 +2,7 @@ import React, { createContext, useContext } from 'react';
 import { useAsapStateStateManager } from './hooks/useAsapStateStateManager';
 import { useAsapAuthStateManager } from './hooks/useAsapAuthStateManager';
 import { useAsapUserStateManager } from './hooks/useAsapUserStateManager';
+import { useAsapDeptHeadStateManager } from './hooks/useAsapDeptHeadStateManager';
 
 const AsapContext = createContext({});
 
@@ -9,11 +10,13 @@ const AsapContextProvider = ({ children }) => {
     const asapStateState = useAsapStateStateManager();
     const asapAuthState = useAsapAuthStateManager();
     const asapUserState = useAsapUserStateManager();
+    const asapDeptHeadState = useAsapDeptHeadStateManager();
 
     const asapStateModel = {
         ...asapStateState,
         ...asapAuthState,
         ...asapUserState,
+        ...asapDeptHeadState,
     };
 
     return <AsapContext.Provider value={asapStateModel}>{children}</AsapContext.Provider>;
