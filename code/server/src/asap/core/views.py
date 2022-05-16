f7faa2b5d0005fac8cb93d8b76106d8bdfdc38d5
itayshlomy <95958637+itayshlomy@users.noreply.github.com>
2022-05-16 13:58:01 +0300

    dept_chair applications filter by admin approved

--- code/server/src/asap/core/views.py
+++ code/server/src/asap/core/views.py
@@ -13,7 +13,8 @@ from core.decorators import authorized_roles
 from core.mail import send_email
 from core.models import Version, Profile, Rank, Application, ApplicationStep, Step
 from core.roles import Role
-from core.serializers import VersionSerializer, ProfileSerializer, RankSerializer, ApplicationSerializer
+from core.serializers import VersionSerializer, ProfileSerializer, RankSerializer, ApplicationSerializer, \
+    ApplicationStepSerializer
 from datetime import date, timedelta
 
 
@@ -96,7 +97,8 @@ def get_dept_head_applications(request):
 @renderer_classes([JSONRenderer])
 @authorized_roles(roles=[Role.ASAP_APPT_CHAIR])
 def get_dept_chair_applications(request):
-    applications = Application.objects.all()
+    steps = ApplicationStep.objects.filter(step_name='ADMIN_VERIFY_APPLICATION')
+    applications = Application.objects.filter(steps__in=steps)
     serializer = ApplicationSerializer(applications, many=True)
     return Response(serializer.data, status=status.HTTP_200_OK)
 
@@ -273,7 +275,7 @@ def submit_admin_application(request, application_id):
             Application.objects.filter(id=application_id).update(application_state=application_state)
             ApplicationStep.objects.update_or_create(
                 application=application, step_name=Step.STEP_3,
-                defaults={'can_update': True, 'can_cancel': True, 'currentStep': True}
+                defaults={'can_update': False, 'can_cancel': True, 'currentStep': True}
             )
             ApplicationStep.objects.update_or_create(
                 application=application, step_name=Step.STEP_1,
