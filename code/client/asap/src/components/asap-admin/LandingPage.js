bf717931629923edccb823c4131297fb2e8f5379
itayshlomy <95958637+itayshlomy@users.noreply.github.com>
2022-05-18 20:39:56 +0300

    building landing page

--- code/client/asap/src/components/asap-admin/LandingPage.js
+++ code/client/asap/src/components/asap-admin/LandingPage.js
@@ -1,6 +1,58 @@
 import React, { useEffect, useState } from 'react';
+import { Button, TextareaAutosize } from '@mui/material';
+import { FormattedMessage, useIntl } from 'react-intl';
+import FormControl from '@mui/material/FormControl';
+import { ASAP_ADMIN_APPLICATIONS } from '../../services/routing/routes';
+import { useNavigate } from 'react-router-dom';
+import rootStyle from '../../style/Asap.module.css';
 
 const LandingPage = () => {
-    return <div></div>;
+    const { formatMessage } = useIntl();
+    const navigate = useNavigate();
+
+    const goToApplications = () => {
+        navigate(`/${ASAP_ADMIN_APPLICATIONS}`);
+    };
+
+    return (
+        <div className={rootStyle.landingPageContainer}>
+            <div>
+                <TextareaAutosize
+                    aria-label="minimum height"
+                    placeholder={formatMessage({ id: 'applications.comments' })}
+                    style={{ width: 100 }}
+                    value={9}
+                    disabled={true}
+                />
+                <FormattedMessage id={'admin.new-applications'} />
+            </div>
+            <div>
+                <TextareaAutosize
+                    aria-label="minimum height"
+                    placeholder={formatMessage({ id: 'applications.comments' })}
+                    style={{ width: 100 }}
+                    value={9}
+                    disabled={true}
+                />
+                <FormattedMessage id={'admin.open-applications'} />
+            </div>
+
+            <div>
+                <TextareaAutosize
+                    aria-label="minimum height"
+                    placeholder={formatMessage({ id: 'applications.comments' })}
+                    style={{ width: 100 }}
+                    value={9}
+                    disabled={true}
+                />
+                <FormattedMessage id={'admin.close-applications'} />
+            </div>
+            <FormControl sx={{ m: 1, minWidth: 120, maxWidth: 120 }}>
+                <Button type="submit" variant="contained" color="success" onClick={goToApplications}>
+                    <FormattedMessage id={'appointment.applications-view'} />
+                </Button>
+            </FormControl>
+        </div>
+    );
 };
 export default LandingPage;
