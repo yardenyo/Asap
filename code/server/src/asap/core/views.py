85c2965c29e3e18eba884e092e256c346cf47842
itayshlomy <95958637+itayshlomy@users.noreply.github.com>
2022-05-27 12:39:56 +0300

    getting close date from last application

--- code/server/src/asap/core/views.py
+++ code/server/src/asap/core/views.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import logout
+from django.db.models import Max
 from rest_framework import status, generics
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
@@ -191,6 +192,15 @@ def submit_dept_head_application(request, application_id):
     if Application.objects.filter(applicant=applicant_profile_id).filter(is_done=0).exists():
         return Response(True, status=status.HTTP_200_OK)
 
+    if Application.objects.filter(applicant=applicant_profile_id).filter(is_done=1).exists():
+        all_close_app_4_applicant = Application.objects.filter(applicant=applicant_profile_id).filter(is_done=1)
+        arg = all_close_app_4_applicant.order_by('-updated_at')[0] #order: from the newest to the oldest
+        last_app_closed = ApplicationStep.objects.filter(application_id=arg.id).filter(step_name="APPLICATION_CLOSE")
+        date_close = last_app_closed[0].created_at
+        toDay = date.today()
+
+        return Response(True, status=status.HTTP_200_OK)
+
     try:
         application = Application.objects.get(id=application_id)
         # TODO - update application
@@ -225,6 +235,12 @@ def submit_dept_head_application(request, application_id):
     sendEmail(addresee, email_headline, wanted_action, applicant, degree)
 
     return Response(False, status=status.HTTP_200_OK)
+
+
+def expired_period_time(applicant_profile_id):
+    app = Application.objects.get(applicant=applicant_profile_id)
+    appl= ApplicationStep.objects.filter(application_id=app.id).filter(step_name="APPLICATION_CLOSE")
+    return 1
 
 
 @api_view(['POST'])
@@ -585,3 +601,8 @@ def sendEmail(mail_addresses, wanted_headline, action_type, name_to_replace=None
     send_email(settings.SENDGRID_SENDER, mail_addresses,
                wanted_headline,
                message)
+
+def expired_period_time():
+
+    return 1
+
